f7230fd65309596720cef2d86fcb20c12cb62ab6
Cesar Augusto <cesar_ags@outlook.com>
2024-01-29 19:56:43 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 // /https://www.mongodb.com/docs/drivers/node/current/quick-start/create-a-connection-string/
 
 module.exports = {
-    connectionString: 'mongodb+srv://cesarags:LgS7gPJkbbQKrg26@cluster0.v9vrwoc.mongodb.net/demo-api?retryWrites=true&w=majority',
-    sendgridKey: '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',
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=nodestoreagsuc;AccountKey=F26YnxF+00reH+Q6FqgzLZguHo6qZQBSX3ZlGFIFEONaxdVqQTDpCF5HdqIB/zQ34SiTSkRz6zF5+AStzy1i3Q==;EndpointSuffix=core.windows.net'
+    connectionString: 'your mongo db connection',
+    sendgridKey: 'your sendgrid key',
+    containerConnectionString: 'your blob storage account connection'
 }
